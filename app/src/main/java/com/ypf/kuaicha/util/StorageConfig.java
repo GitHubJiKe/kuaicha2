6129c026b5b140ff4be922e195916aad73f8e46d
GitHubJiKe <ypf9319@163.com>
2016-03-15 18:17:45 +0800

    commit new code

--- app/src/main/java/com/ypf/kuaicha/util/StorageConfig.java
+++ app/src/main/java/com/ypf/kuaicha/util/StorageConfig.java
@@ -1,7 +1,6 @@
 package com.ypf.kuaicha.util;
 
 import android.os.Environment;
-import android.util.Log;
 
 import java.io.File;
 
@@ -10,68 +9,40 @@ import java.io.File;
  */
 public class StorageConfig {
     public static final String TAG = "StorageConfig";
-    /**
-     * SD卡的目录
-     */
+    /** SD卡的目录 */
     public static String externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
-    /**
-     * 应用的根目录
-     */
-    public static String ROOT = "KuaiCha";
-    /**
-     * 根目录
-     */
+    /** 应用的根目录 */
+    public static String ROOT = "kuaicha";
+    /** 根目录 */
     public static String defaultRootPath = externalStoragePath.concat("/").concat(ROOT).concat("/");
-    /**
-     * 列表缓存目录
-     */
+    /** 列表缓存目录 */
     public static String defaultCachePath = defaultRootPath.concat("/cache/");
-    /**
-     * 图片缓存目录
-     */
+    /** 图片缓存目录 */
     public static String defaultImagePath = defaultRootPath.concat("/image/");
-    /**
-     * gif缓存目录
-     */
-    public static String defaultGifPath = defaultRootPath.concat("/gif/");
-    /**
-     * Sqlite数据库文件位置
-     */
+    /** Sqlite数据库文件位置 */
     public static String defaultSqliteDbPath = defaultRootPath.concat("/sqlite/");
 
-    /**
-     * 创建基础文件夹
-     */
+    /** 创建基础文件夹 */
     public static void mkDirs() {
         //根目录
         File defaultRoot = new File(defaultRootPath);
         if (!defaultRoot.exists()) {
             defaultRoot.mkdirs();
-            Log.d("TAG", "创建文件夹成功");
         }
         //列表缓存目录
         File defaultCache = new File(defaultCachePath);
         if (!defaultCache.exists()) {
             defaultCache.mkdirs();
-            Log.d("TAG", "创建文件夹成功");
         }
         //图片缓存目录
         File defaultImage = new File(defaultImagePath);
         if (!defaultImage.exists()) {
             defaultImage.mkdirs();
-            Log.d("TAG", "创建文件夹成功");
-        }
-        //gif缓存目录
-        File defaultGif = new File(defaultGifPath);
-        if (!defaultGif.exists()) {
-            defaultGif.mkdirs();
-            Log.d("TAG", "创建文件夹成功");
         }
         //Sqlite数据库文件位置
         File defaultSqliteDb = new File(defaultSqliteDbPath);
         if (!defaultSqliteDb.exists()) {
             defaultSqliteDb.mkdirs();
-            Log.d("TAG", "创建文件夹成功");
         }
     }
 }
